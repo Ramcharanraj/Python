682b42c82430cb35434900984d2b3bc1413aea5f
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-12-17 12:35:39 +0530

    Added: Replace String

---
+++ ReplaceString.py
@@ -0,0 +1,10 @@
+"""""
+*author - Ram Chandran
+*date 17-12-2021
+*time - 09:30 AM
+*Title - Replace String
+
+"""""
+
+userName = input("Enter your Name:")
+print("Hello",userName,", How are you ? ")
