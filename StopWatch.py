96fd8021ac3b9f54cc975bcbae76cc545ad96ff8
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-12-21 14:15:12 +0530

    ADDED: StopWatch

--- StopWatch.py
+++ StopWatch.py
@@ -9,9 +9,9 @@ Title : Stop Watch Program
 import time
 def time_convert(sec):
     mins = sec // 60
-    #sec = sec % 60
+   
     hours = mins // 60
-    #mins = mins % 60
+   
 
     print("Time Lapsed = {0}:{1}:{2}".format(int(hours), int(mins), sec))
 
